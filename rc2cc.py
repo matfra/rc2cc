5b8a0faf5dd222cd74a9c9081bb7e5556d79dc7c
mat_fra <mathieu.frappier@gmail.com>
2020-01-25 10:14:40 -0800

    Adding args

--- rc2cc.py
+++ rc2cc.py
@@ -14,9 +14,12 @@ def find_and_connect(cc_name: str) -> pychromecast.Chromecast:
 
 def main():
     parser = argparse.ArgumentParser(description='ZAP remote control for chromecast')
-    parser.add_argument('ccast_name', type=str, nargs='+', help="Name of the Chromecast or Google home you wnat to control the volume of")
+    parser.add_argument('ccast_name', type=str, help="Name of the Chromecast or Google home you want to control the volume of")
+    parser.add_argument('--up', type=int, nargs=1, help="Zap code to increase")
+    parser.add_argument('--down', type=int, nargs=1, help="Zap code to decrease")
     args = parser.parse_args()
 
+    print(args)
     ccast_name=args.ccast_name
     print('Discovering cast devices...', end = ' ')
     cast=find_and_connect(ccast_name)
@@ -33,11 +36,11 @@ def main():
     p = Popen(['/opt/openhab/custom/433Utils/RPi_utils/RFSniffer'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
     for line in p.stdout:
         code = int(line.decode('utf-8').split(' ')[2].rstrip())
-        if code == 5330691: #3 on (left) decrease
+        if code in args.down: #3 on (left) decrease
             current_volume=cast.status.volume_level
             cast.set_volume(max(current_volume - 0.033, 0))
             print('Volume: {}'.format(current_volume))
-        elif code == 5330700: #3 off (right) increase
+        elif code in args.up: #3 off (right) increase
             current_volume=cast.status.volume_level
             cast.set_volume(min(current_volume + 0.02,1))
             print('Volume: {}'.format(current_volume))
